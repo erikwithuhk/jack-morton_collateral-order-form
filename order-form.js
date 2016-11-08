6a69f3b81f91ec75c6dfbed1b667ced0410f55a7
Jonsson <erik_jonsson@jackmorton.com>
2016-11-08 18:27:40 -0500

    create order form class

--- order-form.js
+++ order-form.js
@@ -1,1 +1,23 @@
-console.log('connected');
+function OrderForm() {
+  this.collateralForm = $('.collateral-form');
+  this.total = 0;
+
+  this.renderTotal = function() {
+    var $totalAmountNode = $('.total-amount');
+    $totalAmountNode.html('$' + this.total);
+  }
+  this.setSubmitListener = function() {
+    var that = this;
+    this.collateralForm.submit(function(e) {
+      e.preventDefault();
+      var orderDetails = that.collateralForm.serializeArray();
+      console.log(orderDetails);
+    });
+  }
+
+  this.setSubmitListener();
+}
+
+
+var orderForm = new OrderForm();
+orderForm.renderTotal();
